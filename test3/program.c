6ebe5ae7fda62485a06206d7ab233d7caa385ea1
ziolko <aaartiii99@gmail.com>
2021-03-04 13:40:58 +0100

    Test3-"poprawiony"
    zmieniono nieco wytlumaczenie

--- test3/program.c
+++ test3/program.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include "modul.c"
 
-/* Zmiany: extern -> static , #include "modul.c"  Wtedy zadziała w c ++ */
+/* program nie mógł działać, ponieważ extern w c ++ oznacza definiowanie jakiejś zmiennej "globalnie" tak aby kazdy inny "plik" mógł z niej korzystać,
+ *  o ile ma ten plik zalinkowany. Dla tego w c++, definicja z extern powinna być w pliku modul.c , plik powinien byc zalinkowany, a program.c powinien poprawnie
+ *  korzystac z zadefiniowanych zmiennych.
+ *  */
 static const double PI;
 static const double E;
 
